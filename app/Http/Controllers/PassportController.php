231bbe151d6734afa69da80dae667dce3c89937a
unknown <ljhgg86@163.com>
2020-03-26 09:12:06 +0800

    first commit

---
+++ app/Http/Controllers/PassportController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+
+class PassportController extends Controller
+{
+
+    protected $http;
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->http = new Client();
+    }
+    public function login(Request $request)
+    {
+        $data = array(
+            'username' => request('name'),
+            'password' => request('password'),
+            'scope'    => '',
+            'client_id'     => env('RESERVATION_CLIENT_ID'),
+            'client_secret' => env('RESERVATION_CLIENT_SECRET'),
+            'grant_type'    => 'password'
+        );
+        $response =  $this->http->request('POST',env('APP_URL').'/oauth/token', [
+            'form_params' => $data,
+        ]);
+        $token = json_decode((string)$response->getBody(), true);
+
+        return response()->json([
+            'status'=>true,
+            'data'=>[
+                'successflag' => true,
+                'access_token' => $token['access_token'],
+                'refresh_tOKEN'    => $token['refresh_token'],
+                'expires_in' => $token['expires_in'],
+            ],
+            'message'=>'success',
+
+        ], 200)->cookie('refreshToken', $token['refresh_token'], 14400, null, null, false, true);
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function findForPassport($username)
+    {
+        // filter_var($username, FILTER_VALIDATE_EMAIL) ?
+        //   $credentials['email'] = $username :
+        //   $credentials['name'] = $username;
+
+        // return self::where($credentials)->first();
+        return self::where('name',$username)->first();
+    }
+}
