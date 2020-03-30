926096e9f19734f62f991b0a1c878e3fef12e8d5
unknown <ljhgg86@163.com>
2020-03-30 17:13:18 +0800

    check cors

--- routes/api.php
+++ routes/api.php
@@ -16,16 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-//Route::post('login','PassportController@login');
 Route::prefix('v2')->group(function(){
     Route::post('login','PassportController@login');
-    Route::get('ordertype/orderobject','OrderobjectController@typesObjects')->middleware('auth:api');
-    Route::get('ordertype/{type_id}/orderobject','OrderobjectController@typeObjects')->middleware('auth:api');
-    Route::get('ordertype/orderobject/{object_id','OrderobjectController@object')->middleware('auth:api');
-    Route::get('ordertype/orderobject/{object_id}/date/{date}','OrderobjectController@objectDateTimes')->middleware('auth:api');
     Route::apiResource('ordertype','OrdertypeController')->middleware('auth:api');
-    // Route::get('typesobjects','OrderobjectController@typesObjects')->middleware('auth:api');
-    // Route::get('typeobjects/{type_id}','OrderobjectController@typeObjects')->middleware('auth:api');
-    // Route::get('typeobject/{object_id}','OrderobjectController@object')->middleware('auth:api');
-    // Route::get('objectdate/{object_id}/{date}','OrderobjectController@objectDateTimes')->middleware('auth:api');
+    Route::get('typesobjects','OrderobjectController@typesObjects')->middleware('auth:api');
+    Route::get('typeobjects/{type_id}','OrderobjectController@typeObjects')->middleware('auth:api');
+    Route::get('typeobject/{object_id}','OrderobjectController@object')->middleware('auth:api');
+    Route::get('objectdate/{object_id}/{date}','OrderobjectController@objectDateTimes')->middleware('auth:api');
 });
